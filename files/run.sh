3221e14d048d54429a8dbbcbdb3f72428cdfa339
brastak <brastak@gmail.com>
2020-05-19 12:29:06 +0300

    check run perf tests

---
+++ files/run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Run performance tests.
+#
+# Usage: ./run.sh <jmeter-scenario.jmx> artipie|sonatype [<version>]
+# --------------------------------------------------
+
+function check_prerequisites {
+  type ssh-keygen >/dev/null 2>&1 || { echo >&2 "ssh-keygen is required but is not found"; exit 1; }
+  type terraform >/dev/null 2>&1 || { echo >&2 "terraform is required but is not found"; exit 1; }
+  type ssh >/dev/null 2>&1 || { echo >&2 "ssh is required but is not found"; exit 1; }
+  type scp >/dev/null 2>&1 || { echo >&2 "scp is required but is not found"; exit 1; }
+}
+
+function generate_keys {
+  if [ ! -f "id_rsa_perf" ]
+  then
+    ssh-keygen -t rsa -b 4096 -f id_rsa_perf -N ""
+  fi
+}
+
+if [ $# -lt 2 ]
+then
+  echo >&2 "Usage: ./run.sh <jmeter-scenario.jmx> artipie|sonatype [<version>]"
+  exit 1
+fi
+
+if [ ! -f "$1" ]
+then
+  echo >&2 "Could not find file $1"
+  exit 1
+fi
+scenario=$1
+
+if [ "$2" != 'artipie' ] && [ "$2" != 'sonatype' ]
+then
+  echo >&2 "Unknown repository '$2': only 'artipie' and 'sonatype' are supported"
+  exit 1
+fi
+repository=$2
+
+if [ $# -gt 2 ]
+then
+  version=$3
+else
+  version='latest'
+fi
+
+check_prerequisites
+generate_keys
+terraform init -input=false tf
+terraform apply -input=false -auto-approve \
+  -var "repository={type=\"${repository}\", version=\"${version}\"}" \
+  -var "scenario_file=${scenario}" \
+  tf
+
+jmeter_ip_addr=$(terraform output jmeter_ip_addr)
+server_ip_addr=$(terraform output server_ip_addr)
+
+if [ "${repository}" == "artipie" ]
+then
+  remote_cmd="./apache-jmeter-5.2.1/bin/jmeter -n -t upload-files.jmx -l results.jtl -e -o report -Jrepository.host=${server_ip_addr} -Jrepository.path=files"
+elif [ "${repository}" == "sonatype" ]
+then
+  remote_cmd="./apache-jmeter-5.2.1/bin/jmeter -n -t upload-files.jmx -l results.jtl -e -o report -Jrepository.host=${server_ip_addr} -Jrepository.path='/repository' -Jrepository.path=files -Jlogin=user -Jpassword=password"
+fi
+
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i id_rsa_perf ubuntu@${jmeter_ip_addr} "${remote_cmd}"
+scp -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -i id_rsa_perf -r ubuntu@${jmeter_ip_addr}:report .
+
+terraform destroy -input=false -auto-approve \
+  -var "repository={type=\"${repository}\", version=\"${version}\"}" \
+  -var "scenario_file=${scenario}" \
+  tf
---
+++ files/tf/instances.tf
@@ -0,0 +1,184 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "artipie" {
+  count = var.repository.type == "artipie" ? 1 : 0
+
+  ami = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = aws_key_pair.perf_key_pair.key_name
+  security_groups = [aws_security_group.allow_ssh_sg.id]
+  subnet_id = aws_subnet.perf_subnet.id
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = var.repo_storage_size
+  }
+
+  tags = {
+    Name = "repository",
+    Project = "Artipie Performance"
+  }
+
+  connection {
+    user = "ubuntu"
+    host = self.public_ip
+    private_key = file("id_rsa_perf")
+  }
+
+  provisioner "file" {
+    source = "wait-instance-init.sh"
+    destination = "/home/ubuntu/wait-instance-init.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sh ./wait-instance-init.sh",
+      "sudo apt-get update",
+      "sudo apt-get upgrade -y",
+      "sudo apt-get install -y apt-transport-https gnupg-agent",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+      "mkdir upload"
+    ]
+  }
+
+  provisioner "file" {
+    source = "artipie"
+    destination = "/home/ubuntu/upload"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p artipie/repo",
+      "cp upload/artipie/*.yaml artipie/repo",
+      "sudo docker run -d -v /home/ubuntu/artipie:/var/artipie -p 80:80 --name artipie artipie/artipie:${var.repository.version}",
+      "rm -fr upload"
+    ]
+  }
+}
+
+resource "aws_instance" "sonatype" {
+  count = var.repository.type == "sonatype" ? 1 : 0
+
+  ami = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = aws_key_pair.perf_key_pair.key_name
+  security_groups = [aws_security_group.allow_ssh_sg.id]
+  subnet_id = aws_subnet.perf_subnet.id
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = var.repo_storage_size
+  }
+
+  tags = {
+    Name = "repository",
+    Project = "Artipie Performance"
+  }
+
+  connection {
+    user = "ubuntu"
+    host = self.public_ip
+    private_key = file("id_rsa_perf")
+  }
+
+  provisioner "file" {
+    source = "wait-instance-init.sh"
+    destination = "/home/ubuntu/wait-instance-init.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sh ./wait-instance-init.sh",
+      "sudo apt-get update",
+      "sudo apt-get install -y apt-transport-https gnupg-agent",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+      "mkdir upload",
+      "mkdir nexus-data",
+      "sudo chown 200 nexus-data",
+    ]
+  }
+
+  provisioner "file" {
+    source = "sonatype"
+    destination = "/home/ubuntu/upload"
+  }
+
+  provisioner "file" {
+    content = templatefile("templates/credentials.tpl", {
+      username = var.repository_credentials.username,
+      password = var.repository_credentials.password
+    })
+    destination = "/home/ubuntu/upload/sonatype/credentials"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker run -d -v /home/ubuntu/nexus-data:/nexus-data -p 80:8081 --name nexus sonatype/nexus3:${var.repository.version}",
+      "sudo sh upload/sonatype/wait-nexus-ready.sh",
+      "sudo sed -i '$ a nexus.scripts.allowCreation=true' nexus-data/etc/nexus.properties",
+      "sudo docker restart nexus",
+      "sudo sh upload/sonatype/wait-nexus-ready.sh",
+      "sh upload/sonatype/setup-nexus.sh",
+      "rm -fr upload"
+    ]
+  }
+}
+
+resource "aws_instance" "jmeter" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.perf_key_pair.key_name
+  security_groups = [aws_security_group.allow_ssh_sg.id]
+  subnet_id = aws_subnet.perf_subnet.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "jmeter",
+    Project = "Artipie Performance"
+  }
+
+  connection {
+    user = "ubuntu"
+    host = self.public_ip
+    private_key = file("id_rsa_perf")
+  }
+
+  provisioner "file" {
+    source = var.scenario_file
+    destination = "/home/ubuntu/${var.scenario_file}"
+  }
+
+  provisioner "file" {
+    source = "wait-instance-init.sh"
+    destination = "/home/ubuntu/wait-instance-init.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sh ./wait-instance-init.sh",
+      "sudo apt-get update",
+      "sudo apt-get install -y openjdk-11-jre-headless",
+      "curl -O https://downloads.apache.org/jmeter/binaries/apache-jmeter-5.2.1.tgz",
+      "tar xzf apache-jmeter-5.2.1.tgz"
+    ]
+  }
+}
